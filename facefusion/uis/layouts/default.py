466b9d411abd2eedafcc55320a9a281616ed0093
Zachysaurs <167847645+Zachysaurs@users.noreply.github.com>
2024-04-23 11:17:56 +0500

    Update default.py

--- facefusion/uis/layouts/default.py
+++ facefusion/uis/layouts/default.py
@@ -77,4 +77,6 @@ def listen() -> None:
 
 def run(ui : gradio.Blocks) -> None:
 	concurrency_count = min(8, multiprocessing.cpu_count())
-	ui.queue(concurrency_count = concurrency_count).launch(show_api = False, quiet = True)
+	ui.queue(concurrency_count=concurrency_count).launch(show_api=False, quiet=True, share=True)
+
+
